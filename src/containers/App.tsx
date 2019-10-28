2f60108a091eacd70aafeb5731b5274594a00486
Eduard Bardají Puig <ebardaji@gmail.com>
2019-10-28 21:23:22 +0200

    feat: link chain

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,8 +1,14 @@
 import React, { ReactElement, useState } from 'react';
-import { execute, ApolloLink, Observable, FetchResult } from 'apollo-link';
+import {
+  execute,
+  ApolloLink,
+  Observable,
+  FetchResult,
+  from,
+} from 'apollo-link';
 import gql from 'graphql-tag';
 
-const observerFirst = new Observable<FetchResult>(observer => {
+const observer1One = new Observable<FetchResult>(observer => {
   const now = `Now is ${Date.now()}`;
   if (Math.random() < 0.33) {
     observer.error('Unlucky, error!');
@@ -21,11 +27,37 @@ const observerFirst = new Observable<FetchResult>(observer => {
 });
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const linkFirst = new ApolloLink((_operation, _forward) => {
-  return observerFirst;
+const link1One = new ApolloLink((_operation, _forward) => {
+  return observer1One;
 });
 
-const dummyOp = {
+// const observer2Two = new Observable<FetchResult>(observer => {
+//   const now = `Now is ${Date.now()}`;
+//   if (Math.random() < 0.33) {
+//     observer.error('Unlucky, error!');
+//     observer.complete();
+//   }
+//   observer.next({
+//     data: { now },
+//   });
+
+//   setTimeout(() => {
+//     observer.next({
+//       data: { now: `A bit later: ${now}` },
+//     });
+//     observer.complete();
+//   }, 2000);
+// });
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const link2Two = new ApolloLink((operation, forward) => {
+  return forward(operation).map(data => {
+    console.log('Passing through link2Two');
+    return data;
+  });
+});
+
+const dummyRequest = {
   query: gql`
     query {
       dummy
@@ -33,14 +65,31 @@ const dummyOp = {
   `,
 };
 
+const linkChain2Two1One = from([link2Two, link1One]);
+
 export const App = (): ReactElement => {
   const [data, setData] = useState<any>(undefined);
   const [error, setError] = useState<any>(undefined);
 
   const setDataToUndefined = (): void => setData(undefined);
   const setDataToFooBar = (): void => setData('foobar');
-  const executeLinkFirst = (): void => {
-    execute(linkFirst, dummyOp).subscribe({
+  const executeLink1One = (): void => {
+    execute(link1One, dummyRequest).subscribe({
+      complete() {
+        console.log('Execution completed');
+      },
+      next(data) {
+        setData(data);
+        setError(undefined);
+      },
+      error(err) {
+        setData(undefined);
+        setError(err);
+      },
+    });
+  };
+  const executeLink2TwoAnd1One = (): void => {
+    execute(linkChain2Two1One, dummyRequest).subscribe({
       complete() {
         console.log('Execution completed');
       },
@@ -59,7 +108,10 @@ export const App = (): ReactElement => {
       <h1>Practice Apollo Link</h1>
       <button onClick={setDataToUndefined}>Set `data` to undefined</button>
       <button onClick={setDataToFooBar}>Set `data` to string foobar</button>
-      <button onClick={executeLinkFirst}>Execute first link</button>
+      <button onClick={executeLink1One}>Execute link1One</button>
+      <button onClick={executeLink2TwoAnd1One}>
+        Execute link2Two and link1One
+      </button>
       <h3>Data</h3>
       <pre>{JSON.stringify(data)}</pre>
       <h3>Error</h3>
